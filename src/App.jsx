3452aafbab0e27ead0bfe9bbca16a5a0128ca1ad
Vadim Zhdanov <vadimir29@gmail.com>
2022-12-31 19:26:28 +0700

    Add required componnets and mock data

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppHeader } from './components/app-header/app-header';
+
+export const App = () => {
+  return (
+    <>
+      <AppHeader />
+      <div>App</div>
+    </>
+  );
+};
