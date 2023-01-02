5c4d90f223aefc7bfd292be3b1df66b47037c6b3
Vadim Zhdanov <vadimir29@gmail.com>
2023-01-02 14:28:25 +0700

    Add header styles

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,3 +1,39 @@
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
+
 export const AppHeader = () => {
-  return <div>app-header</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <div className={styles.info}>
+          <div className="mt-4 mb-4">
+            <div className={styles.constructor}>
+              <BurgerIcon />
+              <div>Конструткор</div>
+            </div>
+          </div>
+          <div className="mt-4 mb-4">
+            <div className={styles.orders}>
+              <ListIcon type="secondary" />
+              <div>Лента заказов</div>
+            </div>
+          </div>
+        </div>
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="mt-4 mb-4">
+          <div className={styles.cabinet}>
+            <ProfileIcon />
+            <div>Личный кабинет</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
